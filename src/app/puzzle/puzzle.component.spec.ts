9488cca6cea0b21e5dfc21881d39432ecb051232
Tom <tomwansww@gmail.com>
2022-10-07 00:24:08 +0500

    Implement Puzzle component setCell

--- src/app/puzzle/puzzle.component.spec.ts
+++ src/app/puzzle/puzzle.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Core } from '../core/core';
+import { Cell, CellValue, Column, Row } from '../core/map';
 
 import { PuzzleComponent } from './puzzle.component';
 
@@ -8,9 +10,8 @@ describe('PuzzleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PuzzleComponent ]
-    })
-    .compileComponents();
+      declarations: [PuzzleComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PuzzleComponent);
     component = fixture.componentInstance;
@@ -19,5 +20,45 @@ describe('PuzzleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  const EXPECTED_BUTTON_COUNT = Core.ROW_COUNT * Core.COLUMN_COUNT;
+
+  it(`should render ${EXPECTED_BUTTON_COUNT} puzzle buttons`, () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const puzzleButtons = compiled.querySelectorAll('button.puzzle-item');
+    expect(puzzleButtons?.length).toEqual(EXPECTED_BUTTON_COUNT);
+  });
+
+  it(`should set puzzle items textContent in order`, () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    let value: CellValue = 0;
+    for (let row: Row = 0; row < Core.ROW_COUNT; row++) {
+      for (let col: Column = 0; col < Core.COLUMN_COUNT; col++) {
+        const expectedId = `item${row}${col}`;
+        const item = compiled.querySelector(`#${expectedId}`);
+        expect(item).toBeTruthy();
+        expect(item!.textContent?.trim()).toEqual(value.toString());
+        value++;
+      }
+    }
+  });
+
+  it(`should load puzzle values from Core`, () => {
+    spyOn(component, 'setCell');
+    component.ngOnInit();
+    let value: CellValue = 0;
+    for (let row: Row = 0; row < Core.ROW_COUNT; row++) {
+      for (let col: Column = 0; col < Core.COLUMN_COUNT; col++) {
+        expect(component.setCell).toHaveBeenCalledWith(
+          jasmine.objectContaining({
+            row: row as Row,
+            column: col as Column,
+            value: value as CellValue,
+          })
+        );
+        value++;
+      }
+    }
   });
 });
--- src/app/puzzle/puzzle.component.ts
+++ src/app/puzzle/puzzle.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Core } from '../core/core';
+import { Cell, CellValue, Column, Row } from '../core/map';
+import { Viewable } from '../core/viewable';
 
 @Component({
   selector: 'app-puzzle',
   templateUrl: './puzzle.component.html',
-  styleUrls: ['./puzzle.component.scss']
+  styleUrls: ['./puzzle.component.scss'],
 })
-export class PuzzleComponent implements OnInit {
+export class PuzzleComponent implements OnInit, Viewable {
+  puzzleItems: Cell[] = [];
+  private core: Core;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.core = new Core(this);
   }
 
+  ngOnInit(): void {
+    let value = 0;
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        this.puzzleItems.push({
+          row: row as Row,
+          column: col as Column,
+          value: value as CellValue,
+        });
+        value++;
+      }
+    }
+    this.core.newGame();
+  }
+
+  setCell(cell: Readonly<Cell>): void {
+    const index = cell.row * Core.ROW_COUNT + cell.column;
+    this.puzzleItems[index] = cell;
+  }
 }
